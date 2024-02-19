5353ba3a1f1b3793026239109511238f07a1956f
Denikuras <don.tsyganoff2000@mail.ru>
2024-02-19 20:14:13 +0400

    add 5 task in js

--- hw-2/script.js
+++ hw-2/script.js
@@ -7,3 +7,68 @@ if (quetion === password) {
   alert('Пароль введен неверно');
 }
 // 2 задание
+let c = 2;
+if (c > 0 && c < 10) {
+  console.log('верно');
+} else {
+  console.log('неверно');
+}
+// 3 задание
+let e = 50;
+let d = 12;
+if (e > 100 || d > 100) {
+  console.log('Верно');
+} else {
+  console.log('Неверно');
+}
+// 4 задание
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
+console.log(Number(a) + Number(b));
+// 5 задание
+
+let month = prompt('Введите число');
+
+switch (month) {
+  case '1':
+    console.log('Январь');
+    break;
+  case '2':
+    console.log('Февраль');
+    break;
+  case '3':
+    console.log('Март');
+    break;
+  case '4':
+    console.log('Апрель');
+    break;
+  case '5':
+    console.log('Май');
+    break;
+  case '6':
+    console.log('Июнь');
+    break;
+  case '7':
+    console.log('Июль');
+    break;
+  case '8':
+    console.log('Август');
+    break;
+  case '9':
+    console.log('Сентябрь');
+    break;
+  case '10':
+    console.log('Октябрь');
+    break;
+  case '11':
+    console.log('Ноябрь');
+    break;
+  case '12':
+    console.log('Декабрь');
+    break;
+
+  default:
+    console.log('В году 12 месяцев');
+    break;
+}
