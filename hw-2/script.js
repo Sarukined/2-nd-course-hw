5045d0432511023886631b139f54df7a7b49c2e0
Denikuras <don.tsyganoff2000@mail.ru>
2024-02-19 13:42:28 +0400

    add js 1задание hw-2

---
+++ hw-2/script.js
@@ -0,0 +1,9 @@
+// 1 задание
+let password = 'qwerty';
+let quetion = prompt('Введите пароль');
+if (quetion === password) {
+  alert('Пароль введен верно');
+} else {
+  alert('Пароль введен неверно');
+}
+// 2 задание
